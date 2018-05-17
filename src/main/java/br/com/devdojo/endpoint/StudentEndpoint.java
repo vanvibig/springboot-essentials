916b4c1a361d26329c43fb15b0b6add26a91be65
vanvibig <vanvibig@gmail.com>
2018-05-17 17:33:55 +0700

    Spring Boot Essentials 08 - Adicionando Spring Data JPA com MySQL pt 01

--- src/main/java/br/com/devdojo/endpoint/StudentEndpoint.java
+++ src/main/java/br/com/devdojo/endpoint/StudentEndpoint.java
@@ -2,66 +2,49 @@ package br.com.devdojo.endpoint;
 
 import br.com.devdojo.error.CustomErrorType;
 import br.com.devdojo.model.Student;
-import br.com.devdojo.util.DateUtil;
+import br.com.devdojo.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.xml.ws.Response;
-import java.time.LocalDateTime;
-import java.util.List;
-
-import static java.util.Arrays.asList;
-
 @RestController
 @RequestMapping("students")
 public class StudentEndpoint {
 
-    private final DateUtil dateUtil;
+    private final StudentRepository studentDAO;
 
     @Autowired
-    public StudentEndpoint(DateUtil dateUtil) {
-        this.dateUtil = dateUtil;
+    public StudentEndpoint(StudentRepository studentDAO) {
+        this.studentDAO = studentDAO;
     }
 
-//    @RequestMapping(method = RequestMethod.GET)
     @GetMapping
     public ResponseEntity<?> listAll() {
-//        System.out.println(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
-        return new ResponseEntity<>(Student.studentList, HttpStatus.OK);
+        return new ResponseEntity<>(studentDAO.findAll(), HttpStatus.OK);
     }
 
-//    @RequestMapping(method = RequestMethod.GET, path = "/{id}")
     @GetMapping(path = "/{id}")
-    public ResponseEntity<?> getStudentById(@PathVariable("id") int id) {
-        Student student = new Student();
-        student.setId(id);
-        int index = Student.studentList.indexOf(student);
-
-        if (index == -1) return new ResponseEntity<>(new CustomErrorType("Student not found"), HttpStatus.NOT_FOUND);
-        return new ResponseEntity<>(Student.studentList.get(index), HttpStatus.OK);
-    }
-
-//    @RequestMapping(method = RequestMethod.POST)
-    @PostMapping
-    public ResponseEntity<?> save(@RequestBody Student student) {
-        Student.studentList.add(student);
+    public ResponseEntity<?> getStudentById(@PathVariable("id") Long id) {
+        Student student = studentDAO.findOne(id);
+        if (student == null) return new ResponseEntity<>(new CustomErrorType("Student not found"), HttpStatus.NOT_FOUND);
         return new ResponseEntity<>(student, HttpStatus.OK);
     }
 
-//    @RequestMapping(method = RequestMethod.DELETE)
-    @DeleteMapping
-    public ResponseEntity<?> delete(@RequestBody Student student) {
-        Student.studentList.remove(student);
+    @PostMapping
+    public ResponseEntity<?> save(@RequestBody Student student) {
+        return new ResponseEntity<>(studentDAO.save(student), HttpStatus.OK);
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+        studentDAO.delete(id);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
-//    @RequestMapping(method = RequestMethod.PUT)
     @PutMapping
     public ResponseEntity<?> update(@RequestBody Student student) {
-        Student.studentList.remove(student);
-        Student.studentList.add(student);
+        studentDAO.save(student);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 }
--- src/main/java/br/com/devdojo/model/Student.java
+++ src/main/java/br/com/devdojo/model/Student.java
@@ -1,57 +1,11 @@
 package br.com.devdojo.model;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import javax.persistence.Entity;
 
-import static java.util.Arrays.asList;
+@Entity
+public class Student extends AbstractEntity {
 
-public class Student {
-
-    private int id;
     private String name;
-    public static List<Student> studentList;
-    static {
-        studentRepository();
-    }
-
-    public Student() {
-    }
-
-    public Student(int id, String name) {
-        this(name);
-        this.id = id;
-    }
-
-    public static void studentRepository(){
-        studentList = new ArrayList<>(asList(new Student(1, "Deku"), new Student(2, "Todoroki")));
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Student student = (Student) o;
-        return id == student.id;
-    }
-
-    @Override
-    public int hashCode() {
-
-        return Objects.hash(id);
-    }
-
-    public Student(String name) {
-        this.name = name;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
 
     public String getName() {
         return name;
