079d9d414946a5e6b0374e75358e73a8fa22550c
vanvibig <vanvibig@gmail.com>
2018-05-18 16:23:45 +0700

    Spring Boot Essentials 14 - Tratamento de erros em REST pt 05 - Padronizando todos os erros

--- src/main/java/br/com/devdojo/handler/RestExceptionHandler.java
+++ src/main/java/br/com/devdojo/handler/RestExceptionHandler.java
@@ -1,22 +1,26 @@
 package br.com.devdojo.handler;
 
+import br.com.devdojo.error.ErrorDetails;
 import br.com.devdojo.error.ResourceNotFoundDetails;
 import br.com.devdojo.error.ResourceNotFoundException;
 import br.com.devdojo.error.ValidationErrorDetails;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.Date;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 @ControllerAdvice
-public class RestExceptionHandler {
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<?> handleResourceNotFoundException(ResourceNotFoundException rnfe) {
         ResourceNotFoundDetails rnfDetails = ResourceNotFoundDetails
@@ -31,11 +35,21 @@ public class RestExceptionHandler {
         return new ResponseEntity<>(rnfDetails, HttpStatus.NOT_FOUND);
     }
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<?> handleMethodArgumentNotValidException(MethodArgumentNotValidException manve) {
-        List<FieldError> fieldErrors = manve.getBindingResult().getFieldErrors();
-        String fields = fieldErrors.stream().map(FieldError::getField).collect(Collectors.joining(","));
-        String fieldMessages = fieldErrors.stream().map(FieldError::getDefaultMessage).collect(Collectors.joining(","));
+    @Override
+    public ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex,
+            HttpHeaders headers,
+            HttpStatus status,
+            WebRequest request) {
+        List<FieldError> fieldErrors = ex.getBindingResult().getFieldErrors();
+        String fields = fieldErrors.
+                stream().
+                map(FieldError::getField).
+                collect(Collectors.joining(","));
+        String fieldMessages = fieldErrors.
+                stream().
+                map(FieldError::getDefaultMessage).
+                collect(Collectors.joining(","));
         ValidationErrorDetails rnfDetails = ValidationErrorDetails
                 .Builder
                 .newBuilder()
@@ -43,11 +57,48 @@ public class RestExceptionHandler {
                 .status(HttpStatus.BAD_REQUEST.value())
                 .title("Field Validation Error")
                 .detail("Field Validation Error")
-                .developerMessage(manve.getClass().getName())
+                .developerMessage(ex.getClass().getName())
                 .field(fields)
                 .fieldMessage(fieldMessages)
                 .build();
         return new ResponseEntity<>(rnfDetails, HttpStatus.BAD_REQUEST);
     }
 
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(
+            Exception ex,
+            Object body,
+            HttpHeaders headers,
+            HttpStatus status,
+            WebRequest request) {
+
+        ErrorDetails errorDetails = ErrorDetails
+                .Builder
+                .newBuilder()
+                .timestamp(new Date().getTime())
+                .status(status.value())
+                .title("Internal Exception")
+                .detail(ex.getMessage())
+                .developerMessage(ex.getClass().getName())
+                .build();
+        return new ResponseEntity<>(errorDetails, headers, status);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(
+            HttpMessageNotReadableException ex,
+            HttpHeaders headers,
+            HttpStatus status,
+            WebRequest request) {
+        ErrorDetails errorDetails = ErrorDetails
+                .Builder
+                .newBuilder()
+                .timestamp(new Date().getTime())
+                .status(HttpStatus.NOT_FOUND.value())
+                .title("Resource not found")
+                .detail(ex.getMessage())
+                .developerMessage(ex.getClass().getName())
+                .build();
+        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    }
 }
